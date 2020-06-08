07a440584af8f8acdaf1ee5fb1b30e5fc3362644
Adrián Martínez Martínez <59067547+Jeilbrek@users.noreply.github.com>
2020-06-08 16:15:16 +0200

    added a window

--- 003-HiLoGUI_GuessingGame/src/HiLoGUI.java
+++ 003-HiLoGUI_GuessingGame/src/HiLoGUI.java
@@ -1,4 +1,7 @@
 import javax.swing.*;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class HiLoGUI extends JFrame {
 
@@ -18,12 +21,26 @@ public class HiLoGUI extends JFrame {
             message = guess + " is too high. Try Again.";
         else
             message = guess + " is correct. You win!";
-        
+
         // Show the message to the user, using the lblOutput GUI label
         lblOutput.setText(message);
     }
 
+    public void newGame() {
+        theNumber = (int)(Math.random() * 100 + 1);
+
+        guessButton.addActionListener(new ActionListener() {
+            // @Override
+            public void actionPerformed(ActionEvent e) {
+                checkGuess();
+            }
+        });
+    }
+
     public static void main(String[] args) {
-        // TODO
+        HiLoGUI theGame = new HiLoGUI();
+        theGame.newGame();
+        theGame.setSize(new Dimension(450, 300));
+        theGame.setVisible(true);
     }
 }
